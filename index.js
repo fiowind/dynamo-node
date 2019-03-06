a60c2ff34e29b0ce6948ea97cce840b7b1337da5
fiowind <fiowind.zfx@gmail.com>
2019-03-06 16:07:56 +0800

    update scan bug

--- index.js
+++ index.js
@@ -2,8 +2,29 @@ const AWS = require('aws-sdk');
 const ConditionalQueryBuilder = require('./lib/ConditionalQueryBuilder');
 
 const getPromise = func => (method, params) => new Promise((resolve, reject) => {
-  func[method](params, (err, data) => (err ? reject(err) : resolve(data)));
-});
+  if (method === 'scan') {
+    let allData = []
+    const onScan = (err, data) => {
+      if (err) {
+        console.error("Unable to scan the table. Error JSON:", JSON.stringify(err, null, 2));
+        reject(err)
+      } else {
+        allData = allData.concat(data.Items)
+        if (typeof data.LastEvaluatedKey != "undefined") {
+          params.ExclusiveStartKey = data.LastEvaluatedKey;
+          func[method](params, onScan)
+        }
+        else {
+          resolve(allData)
+        }
+      }
+    }
+    func[method](params, onScan)
+  }
+  else {
+    func[method](params, (err, data) => (err ? reject(err) : resolve(data)))
+  }
+})
 
 // Exports DynamoDB function that returns an object of methods
 module.exports = (conf) => {
@@ -17,7 +38,7 @@ module.exports = (conf) => {
     function () { return new AWS.SharedIniFileCredentials(); },
     function () {
       // if AWS_CONTAINER_CREDENTIALS_RELATIVE_URI is set
-      if(process.env.AWS_CONTAINER_CREDENTIALS_RELATIVE_URI) {
+      if (process.env.AWS_CONTAINER_CREDENTIALS_RELATIVE_URI) {
         return new AWS.ECSCredentials();
       } else {
         return new AWS.EC2MetadataCredentials();
